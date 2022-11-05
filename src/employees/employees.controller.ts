84f782f82821c294b65b9c97b34e037e801dbb20
charith <charith9422@gmail.com>
2022-11-06 04:14:32 +0530

    fix: documentation fix

--- src/employees/employees.controller.ts
+++ src/employees/employees.controller.ts
@@ -12,7 +12,9 @@ import {
 import { EmployeesService } from './employees.service';
 import { CreateEmployeeDto } from './dto/create-employee.dto';
 import { UpdateEmployeeDto } from './dto/update-employee.dto';
+import { ApiTags } from '@nestjs/swagger';
 
+@ApiTags('Employees Module')
 @Controller('employee')
 export class EmployeesController {
   constructor(private readonly employeesService: EmployeesService) {}
