dd42c37677623af0cc52bd3b5d527752bbd5c611
charith <charith9422@gmail.com>
2022-11-07 19:49:22 +0530

    fix: lk validation

--- src/employees/dto/create-employee.dto.ts
+++ src/employees/dto/create-employee.dto.ts
@@ -29,7 +29,7 @@ export class CreateEmployeeDto {
   @ApiProperty({ required: false })
   email: string;
 
-  @IsPhoneNumber()
+  @IsPhoneNumber('LK')
   @ApiProperty({ required: false })
   number: string;
 
