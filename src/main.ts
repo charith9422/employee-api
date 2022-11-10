436898f63f0f4e56e4b58ce32c8f8e25c5219e62
charith <charith9422@gmail.com>
2022-11-10 19:54:40 +0530

    fix: cors fixes

--- src/main.ts
+++ src/main.ts
@@ -4,7 +4,7 @@ import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
 import { AppModule } from './app.module';
 
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule, { cors: true });
+  const app = await NestFactory.create(AppModule);
   app.enableCors({
     allowedHeaders: ['content-type'],
     origin: ['https://employee-manager-app.vercel.app'],
