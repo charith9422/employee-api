4d53a58a3d8f79d6aa28e9b03aefccdbf3961132
charith <charith9422@gmail.com>
2022-11-10 19:36:13 +0530

    fix:cors issue

--- src/main.ts
+++ src/main.ts
@@ -7,7 +7,7 @@ async function bootstrap() {
   const app = await NestFactory.create(AppModule, { cors: true });
   app.enableCors({
     allowedHeaders: ['content-type'],
-    origin: '*f',
+    origin: '*',
     credentials: true,
   });
   app.useGlobalPipes(new ValidationPipe());
